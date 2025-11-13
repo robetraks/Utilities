2f7c79f394e4c126f36517e177083bbb515c6b8d
robetraks <49729643+robetraks@users.noreply.github.com>
2025-11-12 19:04:08 -0500

    Fix package structure and rename to 'utilities'
    - Move utilities.py and __init__.py into utilities/ package directory
    - Rename package from utilities-robetraks to utilities for simpler imports
    - Update setup.py and pyproject.toml with new package name
    - Now imports work correctly: from utilities import ...

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="utilities-robetraks",
+    name="utilities",
     version="0.1.0",
     author="Akshay Jain",
     author_email="jain.akshay@icloud.com",
